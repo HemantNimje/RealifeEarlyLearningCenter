f99d06c3f123cf45b2b6fa2857a4a2a4ad499cc8
Hemant <nimje.hemant@gmail.com>
2016-12-17 17:51:21 -0800

    Add XML for application design. Handle click events to address, contact and email.

--- app/src/main/java/com/example/hemant/realifeearlylearningcenter/MainActivity.java
+++ app/src/main/java/com/example/hemant/realifeearlylearningcenter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.hemant.realifeearlylearningcenter;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView address = (TextView) findViewById(R.id.address);
+        TextView contact = (TextView) findViewById(R.id.contact);
+        TextView email = (TextView) findViewById(R.id.email);
+
+        address.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri mapIntentUri = Uri.parse("geo:42.0694997,-80.156359?q=Realife+Early+Learning+Center");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapIntentUri);
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent callIntent = new Intent(Intent.ACTION_DIAL);
+                callIntent.setData(Uri.parse("tel:8148355178"));
+                if (callIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(callIntent);
+                }
+
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] email = {"pam@rlaerie.com"};
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/html");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, email);
+
+                if (emailIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(emailIntent);
+                }
+            }
+        });
+
     }
 }
